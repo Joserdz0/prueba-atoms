b4df6a9638cc4bb56f86d23431631e8fa88828cf
Jose Antonio Rodriguez Rivera <joserdz052@outlook.com>
2022-07-20 11:40:48 -0500

    Agrego Metodo POST Falta arreglar JSON

--- routes/products.py
+++ routes/products.py
@@ -9,11 +9,20 @@ def index():
     return render_template('index.html')
 @products.route('/new', methods=['POST'])
 def addProduct():
-    name = request.form['name']
-    new_product = Product(name)
+    #Tengo que ver como arreglar el JSON que recibo
+    request_data = request.get_json()
+    name = request_data["name"]
+    sku = request_data["sku"]
+    images = request_data["sku"]
+    tags = request_data["tags"]
+    price = request_data["price"]
+    status = request_data["status"]
+    size = request_data["size"]
+    created_at = ""
+    updated_at = ""
+    new_product = Product(name,sku,images,tags,price,status,size,created_at,updated_at)
     db.session.add(new_product)
     db.session.commit()
-
     return "Creando nuevos"
 @products.route('/update')
 def updateProduct():
